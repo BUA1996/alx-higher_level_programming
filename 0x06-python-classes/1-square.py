a17b47fc2cd639ef92480a3372ff5adbbdd01151
bua <buaisaac17@gail.com>
2023-09-27 00:09:00 -0700

    instantiation

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Defines a class Square"""
+class Square:
+    """Represents a Square class"""
+    def __init__(self, size):
+        """Instantiating"""
+        self.__size = size
